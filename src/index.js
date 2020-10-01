834d0e9add64be0a2d8827a4bee1be55c0e6bfcf
Mihats <misha.ts2104@gmail.com>
2020-10-01 21:56:27 +0300

    feat: end tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    let numbers = [, "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen"
+    ];
+
+    let dozens = [, , "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    let result = "";
+    if (number == 0) return "zero";
+
+    if (number < 20) return numbers[number];
+
+    if (number < 100) {
+
+        if (number % 10 === 0) {
+            return dozens[number / 10];
+        }
+        return dozens[parseInt(number / 10)] + " " + numbers[number % 10];
+    }
+
+    if (number < 1000) {
+
+
+        return number % 100 === 0 ? numbers[parseInt(number / 100)] + " " + "hundred" :
+            number % 100 < 20 ? numbers[parseInt(number / 100)] + " " + "hundred" + " " + numbers[number % 100] :
+            number % 10 === 0 ? numbers[parseInt(number / 100)] + " " + "hundred" + " " + dozens[parseInt((number / 10) % 10)] :
+            numbers[parseInt(number / 100)] + " " + "hundred" + " " + dozens[parseInt((number / 10) % 10)] + " " + numbers[number % 10];
+    }
 }
